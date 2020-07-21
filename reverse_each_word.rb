578d3da765637961e1886209846d941a15eb9d4e
meaghan jennings <meaghanljennings@gmail.com>
2020-07-21 16:49:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,8 @@
 def reverse_each_word(string_argument)
-  string_argument.each do |string|
-    puts string_argument.reverse
+  original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
   end
+  return_array.join(" ")
 end
