f5050aa406600de1d08336240ecbc9b10a26d8f0
meaghan jennings <meaghanljennings@gmail.com>
2020-07-21 16:57:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
-def reverse_each_word(string)
-  original_array = string.split(" ")
+def reverse_each_word(sentence1)
+  original_array = sentence1.split(" ")
   return_array = []
   original_array.each do|string|
     return_array << string.reverse
